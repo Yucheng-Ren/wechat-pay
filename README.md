e1a07bcbeac6f0fc0c59bea38c278e1e90df556b
Narcissist1 <m6106918@gmail.com>
2017-12-19 15:20:50 +0800

    update setup.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # wechat-pay
 wechat pay sdk in python
+
+This is an unofficial sdk for WeChat Pay
+
+install by pip
+
+    pip install wechat-pay
+
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
-  name = 'wechat-pay',
-  packages = ['wechat-pay'],
-  version = '0.1',
-  description = 'A sdk for wechat pay',
-  author = 'Zack Ren',
-  author_email = 'm6106918@gmail.com',
-  url = 'https://github.com/Narcissist1/wechat-pay',
-  download_url = 'https://github.com/Narcissist1/wechat-pay/archive/0.1.tar.gz',
-  keywords = ['wechat', 'pay'],
-  classifiers = [],
+    name='wechat-pay-sdk',
+    packages=find_packages(),
+    version='0.1',
+    description='A sdk for wechat pay',
+    author='Zack Ren',
+    author_email='m6106918@gmail.com',
+    url='https://github.com/Narcissist1/wechat-pay',
+    download_url='https://github.com/Narcissist1/wechat-pay/archive/0.1.tar.gz',
+    keywords=['wechat', 'pay'],
+    classifiers=[],
 )
