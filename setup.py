a33bdbcbb2a72ce582d38b6edd8eacd4a761c7b2
Narcissist1 <m6106918@gmail.com>
2017-12-19 12:09:21 +0800

    rm idea folder

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'wechat-pay',
+  packages = ['wechat-pay'],
+  version = '0.1',
+  description = 'A sdk for wechat pay',
+  author = 'Zack Ren',
+  author_email = 'm6106918@gmail.com',
+  url = 'https://github.com/Narcissist1/wechat-pay',
+  download_url = 'https://github.com/Narcissist1/wechat-pay.git',
+  keywords = ['wechat', 'pay'],
+  classifiers = [],
+)
