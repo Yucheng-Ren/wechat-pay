de0bdf0ca99e225de131c6f476d2c85f9031900e
Narcissist1 <m6106918@gmail.com>
2017-12-19 12:27:02 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Zack Ren',
   author_email = 'm6106918@gmail.com',
   url = 'https://github.com/Narcissist1/wechat-pay',
-  download_url = 'https://github.com/Narcissist1/wechat-pay.git',
+  download_url = 'https://github.com/Narcissist1/wechat-pay/archive/0.1.tar.gz',
   keywords = ['wechat', 'pay'],
   classifiers = [],
 )
